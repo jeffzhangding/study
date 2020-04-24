94d31cf5c1ced9579313c1a1ff317a204b6c98df
wangzhangding <wang. zhangding@chinaredstar.cn>
2020-04-24 18:12:28 +0800

    xx

---  machine_learning/感知机.py
+++  machine_learning/感知机.py
@@ -1,3 +1,171 @@
 __author__ = 'jeff'
 
+"""
+第2章 感知机
+1．感知机是根据输入实例的特征向量$x$对其进行二类分类的线性分类模型：
+
+$$
+f(x)=\operatorname{sign}(w \cdot x+b)
+$$
+感知机模型对应于输入空间（特征空间）中的分离超平面$w \cdot x+b=0$。
+
+2．感知机学习的策略是极小化损失函数：
+
+$$
+\min _{w, b} L(w, b)=-\sum_{x_{i} \in M} y_{i}\left(w \cdot x_{i}+b\right)
+$$
+损失函数对应于误分类点到分离超平面的总距离。
+
+3．感知机学习算法是基于随机梯度下降法的对损失函数的最优化算法，有原始形式和对偶形式。算法简单且易于实现。原始形式中，首先任意选取一个超平面，然后用梯度下降法不断极小化目标函数。在这个过程中一次随机选取一个误分类点使其梯度下降。
+
+4．当训练数据集线性可分时，感知机学习算法是收敛的。感知机算法在训练数据集上的误分类次数$k$满足不等式：
+
+$$
+k \leqslant\left(\frac{R}{\gamma}\right)^{2}
+$$
+当训练数据集线性可分时，感知机学习算法存在无穷多个解，其解由于不同的初值或不同的迭代顺序而可能有所不同。
+
+二分类模型
+$f(x) = sign(w\cdot x + b)$
+
+$\operatorname{sign}(x)=\left\{\begin{array}{ll}{+1,} &amp; {x \geqslant 0} \\ {-1,} &amp; {x&lt;0}\end{array}\right.$
+
+给定训练集：
+
+$T=\left\{\left(x_{1}, y_{1}\right),\left(x_{2}, y_{2}\right), \cdots,\left(x_{N}, y_{N}\right)\right\}$
+
+定义感知机的损失函数
+
+$L(w, b)=-\sum_{x_{i} \in M} y_{i}\left(w \cdot x_{i}+b\right)$
+
+算法
+随即梯度下降法 Stochastic Gradient Descent
+
+随机抽取一个误分类点使其梯度下降。
+
+$w = w + \eta y_{i}x_{i}$
+
+$b = b + \eta y_{i}$
+
+当实例点被误分类，即位于分离超平面的错误侧，则调整$w$, $b$的值，使分离超平面向该无分类点的一侧移动，直至误分类点被正确分类
+
+拿出iris数据集中两个分类的数据和[sepal length，sepal width]作为特征
+
+
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Perceptron
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['label'] = iris.target
+
+
+df.columns = [
+    'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+]
+df.label.value_counts()
+
+plt.scatter(df[:50]['sepal length'], df[:50]['sepal width'], label='0')
+plt.scatter(df[50:100]['sepal length'], df[50:100]['sepal width'], label='1')
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+
+# plt.show()
+
+data = np.array(df.iloc[:100, [0, 1, -1]])
+
+train_data, train_label = data[:, :-1], data[:, -1]
+
+lable = np.array([1 if i == 1 else -1 for i in train_label])
+
+
+class Model(object):
+    """"""
+
+    def __init__(self):
+        self.w = np.ones(len(data[0]) - 1, dtype=np.float32)
+        self.b = 0
+        self.l_rate = 0.1
+
+    def sign(self, x, w, b):
+        y = np.dot(x, w) + b
+        return y
+
+    def model(self):
+        """"""
+
+    def fit(self, x_train, y_train):
+        """"""
+        is_wrong = False
+        n = 0
+        while not is_wrong:
+            n += 1
+            wrong_count = 0
+            for d in range(len(x_train)):
+                x = x_train[d]
+                y = y_train[d]
+                if y * self.sign(x, self.w, self.b) <= 0:
+                    self.w = self.w + self.l_rate * np.dot(y, x)
+                    self.b = self.b + self.l_rate * y
+                    wrong_count += 1
+            if wrong_count == 0:
+                is_wrong = True
+                break
+            print('==训练轮数： %s=== 错误的节点数： %s' % (n, wrong_count))
+        return 'Perceptron Model!'
+
+
+def train():
+    perceptron = Model()
+    perceptron.fit(train_data, lable)
+    x_points = np.linspace(4, 7, 10)
+    y_ = -(perceptron.w[0] * x_points + perceptron.b) / perceptron.w[1]
+    plt.plot(x_points, y_)
+    plt.plot(data[:50, 0], data[:50, 1], 'bo', color='blue', label='0')
+    plt.plot(data[50:100, 0], data[50:100, 1], 'bo', color='orange', label='1')
+    plt.xlabel('sepal length')
+    plt.ylabel('sepal width')
+    plt.legend()
+    plt.show()
+
+
+def skeit_model():
+    """"""
+    p = Perceptron(fit_intercept=True, max_iter=1000, shuffle=True)
+    p.fit(train_data, lable)
+
+    plt.figure(figsize=(10, 10))
+
+    # 中文标题
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.title('鸢尾花线性数据示例')
+
+    plt.scatter(data[:50, 0], data[:50, 1], c='b', label='Iris-setosa', )
+    plt.scatter(data[50:100, 0], data[50:100, 1], c='orange', label='Iris-versicolor')
+
+    # 画感知机的线
+    x_ponits = np.arange(4, 8)
+    y_ = -(p.coef_[0][0] * x_ponits + p.intercept_) / p.coef_[0][1]
+    plt.plot(x_ponits, y_)
+
+    # 其他部分
+    plt.legend()  # 显示图例
+    plt.grid(False)  # 不显示网格
+    plt.xlabel('sepal length')
+    plt.ylabel('sepal width')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    # train()
+    skeit_model()
+
 
