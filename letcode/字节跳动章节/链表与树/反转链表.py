fdbef32dfee5c72d7822fbf1570eb11349b40a83
unknown <18566137534@CRS.PROD>
2020-07-30 19:07:48 +0800

    xx

---
+++ letcode/字节跳动章节/链表与树/反转链表.py
@@ -0,0 +1,36 @@
+__author__ = 'jeff'
+
+"""
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+"""
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        """"""
+        if head is None:
+            return head
+        last = None
+        while 1:
+            if head.next is None:
+                head.next = last
+                break
+            current_head = head
+            head = head.next
+            current_head.next = last
+            last = current_head
+        return head
+
---
+++ letcode/字节跳动章节/链表与树/合并两个有序链表.py
@@ -0,0 +1,55 @@
+__author__ = 'jeff'
+
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+示例：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+"""
+
+# Definition for singly-linked list
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """"""
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        if l1.val <= l2.val:
+            res = ListNode(l1.val)
+            l1 = l1.next
+        else:
+            res = ListNode(l2.val)
+            l2 = l2.next
+        res_head = res
+        while 1:
+            if l1 is None:
+                res_head.next = l2
+                break
+            elif l2 is None:
+                res_head.next = l1
+                break
+
+            if l1.val <= l2.val:
+                current_node = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                current_node = ListNode(l2.val)
+                l2 = l2.next
+            res_head.next = current_node
+            res_head = current_node
+        return res
+
